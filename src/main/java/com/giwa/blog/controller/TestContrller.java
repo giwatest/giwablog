d73910bbb3986e8440c1fd88f0630e6c1087461d
liubingna <giwatest@163.com>
2021-03-24 18:15:29 +0800

    2-8 增加自定义配置项

--- src/main/java/com/giwa/blog/controller/TestContrller.java
+++ src/main/java/com/giwa/blog/controller/TestContrller.java
@@ -1,9 +1,13 @@
 package com.giwa.blog.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestContrller {
+
+    @Value("${test.hello:TEST}")
+    private String testhello;
 
     /**
      *
@@ -11,6 +15,11 @@ public class TestContrller {
      */
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "hello";
+        return "hello " +testhello;
+    }
+
+    @RequestMapping(value = "/hellopost", method = RequestMethod.POST)
+    public String hellopost(String name) {
+        return "hello post" + name;
     }
 }
