5241c54bf95789cdca78c7a451a844111049c75e
liubingna <liubingna@meituan.com>
2021-04-28 23:01:37 +0800

    3.8

--- src/main/java/com/giwa/blog/service/EbookService.java
+++ src/main/java/com/giwa/blog/service/EbookService.java
@@ -5,11 +5,10 @@ import com.giwa.blog.domain.EbookExample;
 import com.giwa.blog.mapper.EbookMapper;
 import com.giwa.blog.req.EbookReq;
 import com.giwa.blog.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.giwa.blog.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,12 +22,7 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+req.getName()+"%");
         List<Ebook> ebooksList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebooksList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        List<EbookResp> respList1 = CopyUtil.copyList(ebooksList, EbookResp.class);
+        return respList1;
     }
 }
